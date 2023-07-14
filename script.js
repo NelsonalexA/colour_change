b35bdf075a40fd6a398fc73e9b20ba3c99e7b784
Nelson alex.A <129277706+NelsonalexA@users.noreply.github.com>
2023-07-14 11:10:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('button');
+const body = document.querySelector('body');
+const color = ['pink','black','red','grey','yellow','purple','green'];
+body.style.backgroundColor='green';
+document.addEventListener('click',function(){
+    const colorIndex = parseInt(Math.random()*color.length);
+    body.style.backgroundColor = color[colorIndex];
+    btn.style.backgroundColor = color[colorIndex];
+});
